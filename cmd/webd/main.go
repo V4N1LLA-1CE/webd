8be19ef3217dbd15600e2f9988fded0f3bb9472b
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-11-02 22:24:16 +1100

    refactor: abstract logic for webp -> png pipeline

--- cmd/webd/main.go
+++ cmd/webd/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/v4n1lla-1ce/webd/internal/cli"
-	"github.com/v4n1lla-1ce/webd/internal/codec"
 	"github.com/v4n1lla-1ce/webd/internal/pipeline"
 )
 
@@ -19,38 +18,11 @@ func main() {
 
 	// get arguments from cli
 	args := cli.GetArgs()
-	ConvertWebpToPNG(args)
-}
 
-func ConvertWebpToPNG(args cli.Arguments) {
+	// handle different conversion options
+	// switch args.Ext {
+	// case ".webp":
+	// }
 
-	fmt.Printf("Starting WebP to PNG conversion\n")
-	fmt.Printf("Source directory: %v\n", args.DirPath)
-	fmt.Printf("Mode: %s\n", map[bool]string{
-		true:  "Converting WebP to PNG and deleting original files",
-		false: "Converting WebP to PNG and preserving original files",
-	}[args.DeleteOrigin])
-	fmt.Println()
-
-	// load data into pipeline
-	files := pipeline.LoadPipeline(args.DirPath, args.DeleteOrigin)
-
-	// decode webp to raw image format
-	decoded := pipeline.NewPipeline(files, codec.DecodeWebp)
-
-	// encode raw image as png
-	encoded := pipeline.NewPipeline(decoded, codec.EncodeToPng)
-
-	// save images to disk
-	saved := pipeline.NewPipeline(encoded, codec.SaveToDisk)
-
-	for result := range saved {
-		if outPath, ok := result.Value.(string); ok {
-			if args.DeleteOrigin {
-				fmt.Printf("Converted WebP to PNG and saved to: %s\n - Deleted original: %v\n", outPath, result.SourcePath)
-			} else {
-				fmt.Printf("Converted WebP to PNG and saved to: %s\n", outPath)
-			}
-		}
-	}
+	pipeline.ConvertWebpToPNG(args)
 }
--- internal/pipeline/pipeline.go
+++ internal/pipeline/pipeline.go
@@ -5,30 +5,27 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/v4n1lla-1ce/webd/internal/cli"
+	"github.com/v4n1lla-1ce/webd/internal/codec"
+	"github.com/v4n1lla-1ce/webd/internal/types"
 )
 
-type PipelineData struct {
-	Value        any
-	SourcePath   string
-	Directory    string
-	BaseName     string
-	DeleteOrigin bool
-}
-
-func LoadPipeline(directory string, deleteOrigin bool) <-chan PipelineData {
-	out := make(chan PipelineData)
+func LoadPipeline(args cli.Arguments) <-chan types.PipelineData {
+	// directory string, deleteOrigin bool
+	out := make(chan types.PipelineData)
 
 	go func() {
 		// close channel once there are no more webp files to be added to pipeline
 		defer close(out)
 
-		entries, err := os.ReadDir(directory)
+		entries, err := os.ReadDir(args.DirPath)
 		if err != nil {
 			fmt.Errorf("Error reading directory: %v\n", err)
 			return
 		}
 
-		// find all webp and add to channel
+		// skip directories
 		for _, entry := range entries {
 			// skip directories
 			if entry.IsDir() {
@@ -36,15 +33,15 @@ func LoadPipeline(directory string, deleteOrigin bool) <-chan PipelineData {
 			}
 
 			// check if webp, if yes, add full path to channel
-			if strings.ToLower(filepath.Ext(entry.Name())) == ".webp" {
-				fullpath := filepath.Join(directory, entry.Name())
+			if strings.ToLower(filepath.Ext(entry.Name())) == args.Ext {
+				fullpath := filepath.Join(args.DirPath, entry.Name())
 				baseName := strings.TrimSuffix(entry.Name(), filepath.Ext(entry.Name()))
 
-				data := PipelineData{
+				data := types.PipelineData{
 					SourcePath:   fullpath,
-					Directory:    directory,
+					Directory:    args.DirPath,
 					BaseName:     baseName,
-					DeleteOrigin: deleteOrigin,
+					DeleteOrigin: args.DeleteOrigin,
 				}
 
 				out <- data
@@ -68,4 +65,37 @@ func NewPipeline[I any, O any](input <-chan I, process func(I) O) <-chan O {
 	}()
 
 	return out
+}
+
+func ConvertWebpToPNG(args cli.Arguments) {
+
+	fmt.Printf("Starting WebP to PNG conversion\n")
+	fmt.Printf("Source directory: %v\n", args.DirPath)
+	fmt.Printf("Mode: %s\n", map[bool]string{
+		true:  "Converting WebP to PNG and deleting original files",
+		false: "Converting WebP to PNG and preserving original files",
+	}[args.DeleteOrigin])
+	fmt.Println()
+
+	// load data into pipeline
+	files := LoadPipeline(args)
+
+	// decode webp to raw image format
+	decoded := NewPipeline(files, codec.DecodeWebp)
+
+	// encode raw image as png
+	encoded := NewPipeline(decoded, codec.EncodeToPng)
+
+	// save images to disk
+	saved := NewPipeline(encoded, codec.SaveToDisk)
+
+	for result := range saved {
+		if outPath, ok := result.Value.(string); ok {
+			if args.DeleteOrigin {
+				fmt.Printf("Converted WebP to PNG and saved to: %s\n - Deleted original: %v\n", outPath, result.SourcePath)
+			} else {
+				fmt.Printf("Converted WebP to PNG and saved to: %s\n", outPath)
+			}
+		}
+	}
 }
